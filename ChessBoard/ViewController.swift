bdeafe9f85476a651e4144eae3082a13f340aacd
Daniel Hoang <thesubway@gmail.com>
2014-09-02 09:52:07 -0700

    Add the chess board

--- ChessBoard/ViewController.swift
+++ ChessBoard/ViewController.swift
@@ -9,10 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    let numRows = 8
+    let numCols = 8
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var greenView = UIView(frame: CGRectMake(50, 50, 200, 400))
+        //        greenView.frame = CGRectMake(50, 50, 200, 400)
+        greenView.backgroundColor = UIColor.greenColor()
+        println(greenView.frame.width)
+        println(greenView.frame.height)
+        println(greenView.frame.origin.x)
+        println(greenView.frame.origin.y)
+        //        self.view.backgroundColor = UIColor.yellowColor() //.CGColor?
+        self.view.backgroundColor = UIColor(red: 200, green: 0, blue: 0, alpha: 1.0)
+        //        self.view.addSubview(colorView)
+        self.view.addSubview(greenView)
+        var chessBoard = UIView()
+        chessBoard.frame = CGRectMake(0, 0, 128, 128)
+        chessBoard.backgroundColor = UIColor.whiteColor()
+        greenView.addSubview(chessBoard)
+        
+        
+        var squareWidth = chessBoard.frame.width / CGFloat(self.numRows)
+        var squareHeight = chessBoard.frame.height / CGFloat(self.numCols)
+        for var i = CGFloat(0); i < CGFloat(self.numRows); i++ {
+            for var j = CGFloat(0); j < CGFloat(self.numCols); j++ {
+                //                let squareXCoord = CGFloat(i) * squareWidth
+                //                let squareYCoord = CGFloat(j) * squareHeight
+                var currentSquare = UIView()
+                //UIButton *firstButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+                
+                currentSquare.frame = CGRectMake(i * squareWidth, j * squareHeight, squareWidth, squareHeight)
+                //now, to check if the square is black or white:
+                if (Int(i) + Int(j)) % 2 == 0 {
+                    currentSquare.backgroundColor = UIColor.whiteColor()
+                }
+                else {
+                    currentSquare.backgroundColor = UIColor.blackColor()
+                }
+                chessBoard.addSubview(currentSquare)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
